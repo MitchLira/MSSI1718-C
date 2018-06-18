969730376b0cf0445df3add49bd5ae0c62300fae
sergioalmeida13 <sergioalmeida26@gmail.com>
2018-06-18 16:31:03 +0100

    for

--- code/src/java/App.java
+++ code/src/java/App.java
@@ -1,5 +1,6 @@
+import java.util.Scanner;
+
 import jason.runtime.*;
-import java.lang.ThreadGroup;
 
 public class App{
 	
@@ -11,11 +12,16 @@ public class App{
 	}
 	
 	public static void main(String[] args) throws InterruptedException {
-		RunJasonProject arg = new RunJasonProject();
-		arg.main(args);
-		Thread t = getThreadByName("MAS-Launcher");
-		t.join();
-		System.out.println("ALOALOALOALOALOALOALOALOALOALOALOALOALOALOALOALOALOALOALOALOALOALOALOALOALOALOALO");
-		arg.main(args);
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Number of simulations:");
+		int n = reader.nextInt();
+		reader.close();
+		for(int i = 0; i < n; i ++) {
+			System.out.println("Running " + i+1 + " simulation...");
+			RunJasonProject.main(args);
+			Thread t = getThreadByName("MAS-Launcher");
+			t.join();
+		}
+	
 	}
 }
